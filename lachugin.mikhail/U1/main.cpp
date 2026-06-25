bf24b58a8408b239c409d7e6a13f4839c83abb7d
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-25 12:37:52 +0300

    try fixxxx

--- lachugin.mikhail/U1/main.cpp
+++ lachugin.mikhail/U1/main.cpp
@@ -5,54 +5,57 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc > 3)
-  {
-    return 1;
-  }
-
   lachugin::Person* persons = nullptr;
   size_t size = 0;
   size_t capacity = 0;
-  std::istream* input = &std::cin;
-  std::ostream* output = &std::cout;
   std::ifstream inputFile;
   std::ofstream outputFile;
+  std::istream* input = &std::cin;
+  std::ostream* output = &std::cout;
+  std::string inputName;
   std::string outputName;
 
-  bool hasInput = false;
-  bool hasOutput = false;
+  size_t inputCount = 0;
+  size_t outputCount = 0;
 
-  for (int i = 1; i < argc; i++)
+  if (argc > 3)
+  {
+    std::cerr << "Too many args\n";
+    return 1;
+  }
+
+  for (int i = 1; i < argc; ++i)
   {
     std::string arg = argv[i];
-    if (arg.size() >= 3 && arg.substr(0, 3) == "in:")
+    if (arg.find("in:") == 0)
     {
-      if (hasInput)
-      {
-        return 1;
-      }
-      inputFile.open(arg.substr(3));
-      if (!inputFile)
-      {
-        return 4;
-      }
-      input = &inputFile;
-      hasInput = true;
+      inputName = arg.substr(3);
+      ++inputCount;
     }
-    else if (arg.size() >= 4 && arg.substr(0, 4) == "out:")
+    else if (arg.find("out:") == 0)
     {
-      if (hasOutput)
-      {
-        return 1;
-      }
-
       outputName = arg.substr(4);
-      hasOutput = true;
+      ++outputCount;
     }
     else
     {
       return 1;
     }
+  }
+
+  if (inputCount > 1 || outputCount > 1)
+  {
+    return 1;
+  }
+  if (inputCount == 1)
+  {
+    inputFile.open(inputName);
+    if (!inputFile)
+    {
+      std::cerr << "Cant open file\n";
+      return 2;
+    }
+    input = &inputFile;
   }
 
   size_t success = 0;
@@ -68,56 +71,44 @@ int main(int argc, char* argv[])
     lachugin::Person person;
     if (!lachugin::parseLine(line, person))
     {
-      ignored++;
+      ++ignored;
       continue;
     }
-
     if (lachugin::containsId(persons, size, person.id))
     {
-      ignored++;
+      ++ignored;
       continue;
     }
 
     lachugin::pushBack(persons, size, capacity, person);
-    success++;
+    ++success;
   }
 
-  if (inputFile.is_open())
-  {
-    inputFile.close();
-  }
-  if (hasOutput)
+  inputFile.close();
+
+  if (outputCount == 1)
   {
     outputFile.open(outputName);
 
     if (!outputFile)
     {
       delete[] persons;
+      std::cerr << "Cant open file\n";
       return 2;
     }
     output = &outputFile;
+  }
+  for (size_t i = 0; i < size; ++i)
+  {
+    *output << persons[i].id << ' ' << persons[i].info << '\n';
   }
 
   if (size == 0)
   {
     *output << '\n';
   }
-  else
-  {
-    for (size_t i = 0; i < size; i++)
-    {
-      *output << persons[i].id << ' ' << persons[i].info << '\n';
-    }
-  }
 
-  if (success != 0 || ignored != 0)
-  {
-    std::cerr << success << ' ' << ignored << '\n';
-  }
-  else
-  {
-    std::cerr << success << ' ' << ignored << '\n';
-  }
+  std::cerr << success << ' ' << ignored << '\n';
   delete[] persons;
   return 0;
 }
